b3a0ec0ecd69e5ce9bc74d58128de6abf728a05e
Prabhudatta Barik <thexas21@gmail.com>
2024-03-19 17:24:01 +0530

    Added router

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,15 @@
 const express = require('express');
+const mainRouter = require("./routes/index");
+const bodyParser = require('body-parser');
+const cors = express();
 
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/api/v1", mainRouter);
+
+
+app.listen(3000, () => {
+    console.log("");
+})
