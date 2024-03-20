9e8dfed4f22acdcb495f7bd0a40cf3c5b95bd9fc
Prabhudatta Barik <thexas21@gmail.com>
2024-03-20 13:16:13 +0530

    added signin

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const PORT = 3000;
 
 const cors = require('cors')
 const mainRouter = require("./routes/index");
@@ -10,6 +11,6 @@ app.use(express.json());
 app.use("/api/v1", mainRouter);
 
 
-app.listen(3000, () => {
-    console.log("Running");
+app.listen(PORT, () => {
+    console.log(`Running on port ${PORT}` );
 })
