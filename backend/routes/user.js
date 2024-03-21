d876e663732f2f6d861f349a2dd89d88be3994ef
Prabhudatta Barik <thexas21@gmail.com>
2024-03-21 11:32:31 +0530

    added features

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -6,6 +6,7 @@ const zod = require('zod');
 const { User } = require('../db');
 const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = require('../config');
+const { authMiddleware } = require('../middleware');
 
 const signUpBody = zod.object({
     username: zod.string().email(),
@@ -13,12 +14,6 @@ const signUpBody = zod.object({
     lastName: zod.string(),
     password: zod.string()
 });
-
-const singinBody = zod.object({
-    username: zod.string().email(),
-    password: zod.string()
-});
-
 
 router.post("/signup", async(req, res) => {
     const { success } = signUpBody.safeParse(req.body)
@@ -54,8 +49,12 @@ router.post("/signup", async(req, res) => {
         message: "User Created Successfully",
         token: token
     })
-})
+});
 
+const singinBody = zod.object({
+    username: zod.string().email(),
+    password: zod.string()
+});
 
 router.post("/signin", async(req, res) => {
     const { success } = singinBody.safeParse(req.body);
@@ -85,6 +84,55 @@ router.post("/signin", async(req, res) => {
     res.status(411).json({
         message: "Error while logging"
     })
-})
+});
+
+const updateBody = zod.object({
+    password: zod.string().optional(),
+    firstName: zod.string().optional(),
+    lasttName: zod.string().optional()
+});
+
+router.put("/", authMiddleware, async(req, res) => {
+    const { success } = updateBody.safeParse(req.body);
+    if(!success) {
+        res.status(411).json({
+            message:"Eror while updating"
+        })
+    }
+
+    await User.updateOne(req.body,{
+        id: req.userId
+    })
+
+    res.json({
+        message: "Updated Sucessfully"
+    })
+});
+
+router.get("/bulk", async(req, res) => {
+    const filter = req.query.filter || "";
+
+    const users = await User.find({
+        $or: [{
+            firstName:{
+                "$regex": filter
+            }
+        }, {
+            lastName:{
+                "$regex": filter
+            }
+        }]
+    })
+
+    res.json({
+        user: users.map(user => ({
+            username: user.username,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            _id: user._id
+        }))
+    })
+});
+
 
 module.exports = router;
