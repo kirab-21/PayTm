103477c5ba54c23115d37f75d4cec04a2639b3a1
Prabhudatta Barik <thexas21@gmail.com>
2024-03-22 11:09:18 +0530

    Final Changes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const account = express.Router();
 
 const userRouter = require('./user');
+const accountRouter = require('./account');
 
 router.use('/user', userRouter);
+router.use('/account', accountRouter);
 
 module.exports = router;
